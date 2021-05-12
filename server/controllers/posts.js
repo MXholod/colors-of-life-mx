60a8fe45104bbdde23e9d370b7b6c17b7b6b6926
Michael Holod <git_ara@ukr.net>
2021-05-12 18:58:00 +0300

    Route and Controller to create posts are ready to use

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -10,3 +10,16 @@ export const getPosts = async (req, res)=>{
         res.status(404).json({ message: e.message });
     }
 }
+
+export const createPost = async (req, res)=>{
+    const post = req.body;
+    const newPost = new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+    }catch(e){
+        //The HTTP 409 Conflict response status code indicates a request conflict with current state of the target resource
+        res.status(409).json({ message: e.message });
+    }
+    res.send('Post creation');
+}
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getPosts } from './../controllers/posts';
+import { getPosts, createPost } from './../controllers/posts';
 
 const router = express.Router();
 
 // http://localhost:5000/posts
 router.get('/', getPosts);
+// http://localhost:5000/posts
+router.post('/', createPost);
 
 export default router;
