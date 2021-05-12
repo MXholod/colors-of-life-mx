98c5b2115bb4bd564d83620c00840767708bc7b8
Michael Holod <git_ara@ukr.net>
2021-05-12 18:27:09 +0300

    Two packages of Babel were installed to support async/await functions

--- server/entry_point.js
+++ server/entry_point.js
@@ -1,7 +1,10 @@
 //This allows to use ES Modules instead of CommonJS
 // Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
 require("@babel/register")({
-    presets: ["@babel/preset-env"]
+    presets: ["@babel/preset-env"],
+    plugins: [
+        ["@babel/transform-runtime"]
+    ]
 });
   
 // Import the entry point of our Application (rest of our application).
