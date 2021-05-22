309004ebb16eb8e51b4dc113c289805d10204421
Michael Holod <git_ara@ukr.net>
2021-05-23 01:52:13 +0300

    The component 'Post' outputs data from the state

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = ()=>{
     const classes = useStyles();
+    //The state is the whole global Redux store
+    const posts = useSelector((state)=>{
+        //'posts' - from 'combineReducers({ posts: posts })' where it's a key 
+        return state.posts;
+    });
+    console.log("Posts ",posts);
     return (
         <>
             <h1>Posts</h1>
