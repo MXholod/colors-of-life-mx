33e190e13b6bc473b0fd763ce95e8458f8bc3b1e
Michael Holod <git_ara@ukr.net>
2021-05-23 00:51:07 +0300

    Let's change the main file index.js and add path to 'reducers' folder and also add 'redux', 'react-redux', 'redux-thunk' to work with asynchronous store

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
+//Import Reducers
+import reducers from './reducers';
+//Set Redux Store
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider store={ store }>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
