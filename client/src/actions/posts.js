94398d86020233f3522eadcbef9abf81c551074a
Michael Holod <git_ara@ukr.net>
2021-05-23 01:41:48 +0300

    'action' folder contains the first action creator 'posts.js' that was placed in it

---
+++ client/src/actions/posts.js
@@ -0,0 +1,36 @@
+import * as api from './../api';
+
+/*
+//Action creator without 'redux-thunk'
+const getPosts = ()=> {
+    const action = { type: 'FETCH_ALL', payload: [] };
+    return action;
+}
+//With 'redux-thunk'
+const getPosts = ()=> async (dispatch)=> {
+    const action = { type: 'FETCH_ALL', payload: [] };
+    return action;
+}
+  The problem is we are working with asynchronous data to actually fetch all
+the posts that's why we should use 'redux-thunk'
+  'redux-thunk' allows us to use the 'async / await' capabilities, it wraps 
+the content of the 'action cretor' with a function, this function contains 
+'dispatch'
+*/
+//Action Creators
+export const getPosts = ()=> async (dispatch)=> {
+    try{
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    }catch(e){
+        console.log(e.message);
+    }
+}
+export const createPost = (post)=> async (dispatch)=> {
+    try{
+        const { data } = await api.createPost(post);
+        dispatch({ type: 'CREATE_POST', payload: data });
+    }catch(e){
+        console.log(e.message);
+    }
+}
