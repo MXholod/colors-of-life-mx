71467f0ec31926756ffddca9f3c6b0a3e511b399
Michael Holod <git_ara@ukr.net>
2021-05-25 02:37:10 +0300

    Reducer to update one of the post is created

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,11 +1,12 @@
 const initialState = [];
 
 const posts = (state = initialState, action)=>{
-    switch(action.type){
-        case "FETCH_ALL": return action.payload;
-        case "CREATE_POST": return [ ...state, action.payload ];
-        default: return state;
-    }
+  switch(action.type){
+    case "FETCH_ALL": return action.payload;
+    case "CREATE_POST": return [ ...state, action.payload ];
+    case "UPDATE_POST": return state.map( post => post._id === action.payload._id ? action.payload : post);
+    default: return state;
+  }
 }
 
 export default posts;
