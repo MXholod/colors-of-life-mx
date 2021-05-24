ba395ae368503040321bee84c1c8b7481615d499
Michael Holod <git_ara@ukr.net>
2021-05-25 02:24:30 +0300

    The Model method 'findOneAndUpdate' has been corrected. One of its parameter was changed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import PostMessage from './../models/postMessage';
 
 export const getPosts = async (req, res)=>{
@@ -21,5 +22,17 @@ export const createPost = async (req, res)=>{
         //The HTTP 409 Conflict response status code indicates a request conflict with current state of the target resource
         res.status(409).json({ message: e.message });
     }
-    res.send('Post creation');
 }
+
+export const updatePost = async (req, res)=>{
+    const { id: _id } = req.params;
+    const post = req.body;
+    //Checking '_id' is mongoose id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post');
+    try{
+        const updatedPost = await PostMessage.findOneAndUpdate(_id, { ...post, _id }, { new : true });
+        res.status(200).json(updatedPost);
+    }catch(e){
+        res.status(404).json({ message: e.message });
+    }
+};
