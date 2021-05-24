7c43ab214fa8968978cd0e4b9b5a716f4bfe18e5
Michael Holod <git_ara@ukr.net>
2021-05-25 02:42:32 +0300

    'App' component has obtained a state with 'id' that pass to the children components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { getPosts } from './actions/posts';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
@@ -11,10 +11,12 @@ const App = ()=>{
   const classes = useStyles();
   const dispatch = useDispatch();
 
+  const [currentId, setCurrentId] = useState(null);
+
   useEffect(()=>{
     //Pass getPosts() to the Reducer
     dispatch(getPosts());
-  },[dispatch]);
+  },[currentId, dispatch]);
 
   return (
     <Container maxWidth="lg">
@@ -26,10 +28,10 @@ const App = ()=>{
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={ setCurrentId } />
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={ currentId } setCurrentId={ setCurrentId } />
             </Grid>
           </Grid>
         </Container>
