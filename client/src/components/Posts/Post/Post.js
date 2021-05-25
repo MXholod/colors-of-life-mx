6923c096e429c452c7df655e5100193a79e628c8
Michael Holod <git_ara@ukr.net>
2021-05-25 16:53:22 +0300

    The Post component is removed by clicking on the delete button

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,8 +5,11 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { deletePost } from './../../../actions/posts';
 
 const Post = ({ post, setCurrentId })=>{
+  const dispatch = useDispatch();
   const classes = useStyles();
     return (
       <Card className={ classes.card }>
@@ -46,7 +49,9 @@ const Post = ({ post, setCurrentId })=>{
             <ThumbUpAltIcon fontSize="small" />
             Like { post.likeCount }
           </Button>
-          <Button size="small" color="primary" onClick={ ()=>{} }>
+          <Button size="small" color="primary" onClick={ ()=>{ 
+            dispatch(deletePost(post._id));
+          } }>
             <DeleteIcon fontSize="small" />
             Delete
           </Button>
