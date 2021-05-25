e65cca978fdbe963528bc1a475c5330297ddc16f
Michael Holod <git_ara@ukr.net>
2021-05-25 16:40:14 +0300

    The Route to delete a post is created

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost, updatePost } from './../controllers/posts';
+import { getPosts, createPost, updatePost, deletePost } from './../controllers/posts';
 
 const router = express.Router();
 
@@ -9,5 +9,7 @@ router.get('/', getPosts);
 router.post('/', createPost);
 // http://localhost:5000/posts/12
 router.patch('/:id', updatePost);
+// http://localhost:5000/posts/12
+router.delete('/:id', deletePost);
 
 export default router;
