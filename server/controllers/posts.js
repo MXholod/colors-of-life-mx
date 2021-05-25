325481154b52bb409fc083a75b3b5196b5dc6a54
Michael Holod <git_ara@ukr.net>
2021-05-25 16:41:08 +0300

    The Controller to delete a post is created

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -36,3 +36,15 @@ export const updatePost = async (req, res)=>{
         res.status(404).json({ message: e.message });
     }
 };
+
+export const deletePost = async (req, res)=>{
+    const { id: _id } = req.params;
+    //Checking '_id' is mongoose id
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post');
+    try{
+        await PostMessage.findByIdAndRemove(_id);
+        res.status(200).json({ message: "Post deleted successfully" });
+    }catch(e){
+        res.status(404).json({ message: e.message });
+    }
+};
