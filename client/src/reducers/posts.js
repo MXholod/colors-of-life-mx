0f883bf9d72b2126d00e247895b3e8d701be7944
Michael Holod <git_ara@ukr.net>
2021-05-25 22:03:59 +0300

    The Reducer which uses 'action creator' to like a post on the client side was created

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -5,6 +5,7 @@ const posts = (state = initialState, action)=>{
     case "FETCH_ALL": return action.payload;
     case "CREATE_POST": return [ ...state, action.payload ];
     case "UPDATE_POST": return state.map( post => post._id === action.payload._id ? action.payload : post);
+    case "LIKE_POST": return state.map( post => post._id === action.payload.postUpdated._id ? action.payload.postUpdated : post);
     case "DELETE_POST": return state.filter( post => post._id !== action.payload ? true : false);
     default: return state;
   }
