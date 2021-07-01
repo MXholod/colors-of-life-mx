de901c786bae7d94a2cde289542cb0cc4ac9d955
Michael Holod <git_ara@ukr.net>
2021-07-02 01:22:26 +0300

    Route 'posts' are protected by auth middleware that is checking JWT

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,17 +7,19 @@ import {
     likePost
 } from './../controllers/posts';
 
+import auth from './../middleware/auth';
+
 const router = express.Router();
 
 // http://localhost:5000/posts
 router.get('/', getPosts);
 // http://localhost:5000/posts
-router.post('/', createPost);
+router.post('/', auth, createPost);
 // http://localhost:5000/posts/12
-router.patch('/:id', updatePost);
+router.patch('/:id', auth, updatePost);
 // http://localhost:5000/posts/12
-router.delete('/:id', deletePost);
+router.delete('/:id', auth, deletePost);
 // http://localhost:5000/posts/12/likePost
-router.patch('/:id/likePost', likePost);
+router.patch('/:id/likePost', auth, likePost);
 
 export default router;
