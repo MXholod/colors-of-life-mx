01c8783cd263fd3d826e15fcdce032a50cb58c08
Michael Holod <git_ara@ukr.net>
2021-07-02 01:27:49 +0300

    Controllers 'createPost' and 'likePost' were changed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -14,7 +14,7 @@ export const getPosts = async (req, res)=>{
 
 export const createPost = async (req, res)=>{
     const post = req.body;
-    const newPost = new PostMessage(post);
+    const newPost = new PostMessage({ ...post, creator: req.userId, createdAt: new Date().toISOString() });
     try{
         await newPost.save();
         res.status(201).json(newPost);
@@ -54,11 +54,22 @@ export const deletePost = async (req, res)=>{
 
 export const likePost = async (req, res)=>{
     const { id: _id } = req.params;
+    if(!req.userId) return res.status(401).json({ message: "Unathorized" });
     //Checking '_id' is mongoose id
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('There is no post');
     try{
         const post = await PostMessage.findById(_id);
-        const postUpdated = await PostMessage.findByIdAndUpdate(_id, { likeCount: (post.likeCount + 1) }, { new: true });
+        //Check if user is already liked a post
+        const index = post.likes.findIndex(id => id === String(req.userId));
+        //If a person has not liked the post yet
+        if(index === -1){
+            post.likes.push(req.userId);
+        }else{
+            //Remove person's like
+            post.likes = post.likes.filter(id => id !== String(req.userId));
+        }
+        //The 'post' beneath is instead of this - { likeCount: (post.likeCount + 1) }
+        const postUpdated = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
         res.status(200).json({ message: "Post updated successfully", postUpdated });
     }catch(e){
         res.status(404).json({ message: e.message });
