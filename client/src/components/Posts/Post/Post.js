dd8e61c614ec369208f87dcd67c60e51a26704a8
Michael Holod <git_ara@ukr.net>
2021-07-02 21:08:04 +0300

    The showing of the delete post button depends on who the creator of the post is

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -55,12 +55,14 @@ const Post = ({ post, setCurrentId })=>{
           } }>
             <Likes user={user} post={post} />
           </Button>
-          <Button size="small" color="primary" onClick={ ()=>{ 
-            dispatch(deletePost(post._id));
-          } }>
-            <DeleteIcon fontSize="small" />
-            Delete
-          </Button>
+          { (user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+            <Button size="small" color="primary" onClick={ ()=>{ 
+              dispatch(deletePost(post._id));
+            } }>
+              <DeleteIcon fontSize="small" />
+              Delete
+            </Button>
+          )}
         </CardActions>
       </Card>
   );
