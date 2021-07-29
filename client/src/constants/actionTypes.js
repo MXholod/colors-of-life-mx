dfd30963197da262416e5ce07f62ef9e0dc6e397
Michael Holod <git_ara@ukr.net>
2021-07-29 19:23:01 +0300

    Action type 'FETCH_BY_SEARCH' has been added

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -2,6 +2,7 @@ export const CREATE_POST = 'CREATE_POST';
 export const UPDATE_POST = 'UPDATE_POST';
 export const DELETE_POST = 'DELETE_POST';
 export const FETCH_ALL_POST = 'FETCH_ALL_POST';
+export const FETCH_BY_SEARCH = 'FETCH_BY_SEARCH';
 export const LIKE_POST = 'LIKE_POST';
 
 export const AUTH = 'AUTH';
