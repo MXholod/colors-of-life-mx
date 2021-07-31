59ceb2fe9e8f86800f2400a68c4c3ba21b6dcf74
Michael Holod <git_ara@ukr.net>
2021-07-31 23:52:05 +0300

    Action creator obtaining a post by 'id' through the API then dispatch the data into the reducer

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,6 +1,7 @@
 import * as api from './../api';
 import { 
     FETCH_ALL_POST,
+    FETCH_POST,
     CREATE_POST,
     UPDATE_POST,
     DELETE_POST,
@@ -34,6 +35,16 @@ const unauthorized = (e, dispatch, history)=>{
     }
 };
 //Action Creators
+export const getPost = (id)=> async (dispatch)=> {
+    try{
+        dispatch({ type: START_LOADING });
+        const { data } = await api.fetchPost(id);
+        dispatch({ type: FETCH_POST, payload: data.post });
+        dispatch({ type: END_LOADING });
+    }catch(e){
+        //console.log(e.message);
+    }
+}
 export const getPosts = (page)=> async (dispatch)=> {
     try{
         dispatch({ type: START_LOADING });
