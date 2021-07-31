137866ec07f6de96317d4e2207af4c8f171e8b6f
Michael Holod <git_ara@ukr.net>
2021-07-31 23:45:56 +0300

    API to obtain a post by 'id'

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -25,6 +25,8 @@ API.interceptors.response.use(function (response) {
 });*/
 
 //Posts
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+
 export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 
 export const fetchPostBySearch = (searchQuery) => API.get(`/posts/search?searchQuery=${searchQuery.search || 'none'}&tags=${searchQuery.tags}`);
