893a57611fc74b4f5a8ad4d7d3409d5c9b5a32ea
Michael Holod <git_ara@ukr.net>
2021-07-31 15:08:09 +0300

    Reducer has been changed. Its two cases 'FETCH_ALL_POST' and 'FETCH_BY_SEARCH' were fixed

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -11,12 +11,18 @@ const initialState = [];
 
 const posts = (state = initialState, action)=>{
   switch(action.type){
-    case FETCH_ALL_POST: return action.payload;
+    case FETCH_ALL_POST: 
+    return {
+      ...state,
+      posts: action.payload.data,
+      currentPage: action.payload.currentPage,
+      numberOfPages: action.payload.numberOfPages
+    };
     case CREATE_POST: return [ ...state, action.payload ];
     case UPDATE_POST: return state.map( post => post._id === action.payload._id ? action.payload : post);
     case LIKE_POST: return state.map( post => post._id === action.payload.postUpdated._id ? action.payload.postUpdated : post);
     case DELETE_POST: return state.filter( post => post._id !== action.payload ? true : false);
-    case FETCH_BY_SEARCH: return action.payload;
+    case FETCH_BY_SEARCH: return { ...state, posts: action.payload };
     default: return state;
   }
 }
