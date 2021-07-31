f999ba9cf26cbbe1947b97b17eef4ea30a9c2a7d
Michael Holod <git_ara@ukr.net>
2021-07-31 20:08:23 +0300

    Two constants 'START_LOADING' and 'END_LOADING' are applied to reducer and the state of reducer has been refactored from array to an object

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -4,13 +4,19 @@ import {
   UPDATE_POST,
   DELETE_POST,
   LIKE_POST,
-  FETCH_BY_SEARCH
+  FETCH_BY_SEARCH,
+  START_LOADING,
+  END_LOADING
 } from './../constants/actionTypes';
 
-const initialState = [];
+const initialState = { isLoading: true, posts: [] };
 
 const posts = (state = initialState, action)=>{
   switch(action.type){
+    case START_LOADING:
+      return { ...state, isLoading: true };
+    case END_LOADING:
+      return { ...state, isLoading: false };
     case FETCH_ALL_POST: 
     return {
       ...state,
@@ -18,10 +24,10 @@ const posts = (state = initialState, action)=>{
       currentPage: action.payload.currentPage,
       numberOfPages: action.payload.numberOfPages
     };
-    case CREATE_POST: return [ ...state, action.payload ];
-    case UPDATE_POST: return state.map( post => post._id === action.payload._id ? action.payload : post);
-    case LIKE_POST: return state.map( post => post._id === action.payload.postUpdated._id ? action.payload.postUpdated : post);
-    case DELETE_POST: return state.filter( post => post._id !== action.payload ? true : false);
+    case CREATE_POST: return { ...state, posts: [ ...state.posts, action.payload] };
+    case UPDATE_POST: return { ...state, posts: state.posts.map( post => post._id === action.payload._id ? action.payload : post) };
+    case LIKE_POST: return { ...state, posts: state.posts.map( post => post._id === action.payload.postUpdated._id ? action.payload.postUpdated : post) };
+    case DELETE_POST: return {...state, posts:  state.posts.filter( post => post._id !== action.payload ? true : false) };
     case FETCH_BY_SEARCH: return { ...state, posts: action.payload };
     default: return state;
   }
